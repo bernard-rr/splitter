00ec58073b370142f4f645eac6b3cc4cda2000f5
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-05-25 12:13:11 +0000

    update

--- app.py
+++ app.py
@@ -29,22 +29,32 @@ def main():
                 st.write("PDF splitting complete.")
 
                 # Display the split PDF files
-                file_names = [file for file in os.listdir() if file.startswith("split_") and file.endswith(".pdf")]
+                file_names = []
+
+                if start_page is None:
+                    file_names = [file for file in os.listdir() if file.startswith("split_") and file.endswith(".pdf")]
+                else:
+                    for file in os.listdir():
+                        if file.endswith(".pdf"):
+                            page_number = int(file.split("_")[1].split(".")[0])
+                            if page_number >= start_page:
+                                file_names.append(file)
+
                 if file_names:
                     st.write("Split files:")
                     for file_name in file_names:
                         st.write(file_name)
-                    
+
                     # Create a zip file containing the split files
                     with ZipFile("split_files.zip", "w") as zip:
                         for file_name in file_names:
                             zip.write(file_name)
-                    
+
                     # Provide a download link for the zip file
                     st.markdown("[Download all files](split_files.zip)")
                 else:
                     st.write("No files were split.")
-                
+
 
 if __name__ == "__main__":
     main()
