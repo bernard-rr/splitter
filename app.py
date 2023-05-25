77467506d07dfd751dd1cb53c94649ffb6f477fd
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-05-25 15:40:43 +0000

    VV3

--- app.py
+++ app.py
@@ -17,16 +17,31 @@ def main():
             tmp.write(uploaded_file.getvalue())
             tmp_file_path = tmp.name
 
+            original_file_name = uploaded_file.name
+            uploaded_file_path = os.path.join(tempfile.gettempdir(), original_file_name)
+            with open(uploaded_file_path, "wb") as file:
+                file.write(uploaded_file.getvalue())
+
             intervals_input = st.text_input("Enter the number of pages per file (optional):")
             start_page_input = st.text_input("Enter the start page for title extraction (optional):")
             start_line_input = st.text_input("Enter the start line on the selected page for title extraction (optional):")
 
             if st.button("Split"):
                 intervals = int(intervals_input) if intervals_input else 1
-                start_page = int(start_page_input) if start_page_input else None
-                start_line = int(start_line_input) if start_line_input else None
 
-                split_pdf(tmp_file_path, intervals, start_page, start_line)
+                try:
+                    start_page = int(start_page_input) if start_page_input else None
+                except ValueError:
+                    st.write("Invalid start page input. Setting start page to None.")
+                    start_page = None
+
+                try:
+                    start_line = int(start_line_input) if start_line_input else None
+                except ValueError:
+                    st.write("Invalid start line input. Setting start line to None.")
+                    start_line = None
+
+                split_pdf(uploaded_file_path, intervals, start_page, start_line)
 
                 st.write("PDF splitting complete.")
 
@@ -52,7 +67,10 @@ def main():
                         zip_file_path = os.path.join(temp_dir, "split_files.zip")
                         with ZipFile(zip_file_path, "w") as zip:
                             for file_name in file_names:
-                                zip.write(file_name)
+                                original_name = os.path.splitext(uploaded_file.name)[0]
+                                page_range = file_name.split("_")[1].split(".")[0]
+                                new_name = f"{original_file_name}_split_{page_range}.pdf"
+                                zip.write(file_name, arcname=new_name)
 
                         # Provide a download button for the zip file
                         st.download_button("Download all files", data=open(zip_file_path, "rb"), file_name="split_files.zip")
