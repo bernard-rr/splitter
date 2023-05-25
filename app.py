054c42506b6249dbc78695e6b58149124e910448
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-05-25 13:29:45 +0000

    updatex_3

--- app.py
+++ app.py
@@ -51,7 +51,10 @@ def main():
                         zip_file_path = os.path.join(temp_dir, "split_files.zip")
                         with ZipFile(zip_file_path, "w") as zip:
                             for file_name in file_names:
-                                zip.write(file_name)
+                                original_name = os.path.splitext(uploaded_file.name)[0]
+                                page_range = file_name.split("_")[1].split(".")[0]
+                                new_name = f"{original_name}_split_{page_range}.pdf"
+                                zip.write(file_name, arcname=new_name)
 
                         # Provide a download button for the zip file
                         st.download_button("Download all files", data=open(zip_file_path, "rb"), file_name="split_files.zip")
